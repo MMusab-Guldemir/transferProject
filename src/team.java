e11008811eb90fc4feaa9dc93d36f0b03dbcc074
Muhammed Musab Güldemir <mmusabgldmr@gmail.com>
2026-01-02 15:26:28 +0300

    .
    .

---
+++ src/team.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.io.NotActiveException;
+import java.net.ContentHandler;
+import java.util.ArrayList;
+
+public class team {
+    int ID;
+    String name;
+    int avarageAge;
+    int marketValue;
+    ArrayList<player> squad = new ArrayList<>();
+
+    public team(int ID, String name, int avarageAge, int marketValue, ArrayList squad) {
+        this.ID = ID;
+        this.name = name;
+        this.avarageAge = avarageAge;
+        this.marketValue = marketValue;
+        this.squad = squad;
+    }
+
+    public static boolean playerExist(){
+        if ()
+    }
+}
